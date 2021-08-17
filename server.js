0a99553249fd892b62468d47a1904c3fd646a000
NayanSidhu <nsidh068@uottawa.ca>
2021-08-17 02:45:23 -0400

    bug fixes

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ const initiateApp = () => {
 
     //To display ALL Employees
     displayEmployees = () => {
-        const query = "SELECT employee.id, employee.first_name AS 'First Name', employee.last_name AS 'Last Name', employee_role.title AS 'Job Designation', employee_role.salary AS Salary, department.department_name FROM department INNER JOIN employee_role ON employee_role.department_id = department.id INNER JOIN employee ON employee.id = employee_role.id"
+        const query = "SELECT employee.id, employee.first_name AS 'First Name', employee.last_name AS 'Last Name', employee_role.title AS 'Job Designation', employee_role.salary AS Salary, department.department_name FROM department INNER JOIN employee_role ON employee_role.department_id = department.id INNER JOIN employee ON employee.id = employee_role.id INNER JOIN employee manager on manager.id = employee.manager_id"
        connection.query(query,
             function (err, res){
                 if (err) throw err;
@@ -138,7 +138,7 @@ const initiateApp = () => {
                 console.log(answers);
                 console.log('\n_______Adding the new Department_______\n');
                 const query = connection.query(
-                    `SELECT id AS value, department_name AS Department FROM department`, answers,
+                    `INSERT INTO department SET ?`, answers,
                     function (err, res){
                         if (err) throw err; 
                         console.log(res.affectedRows);
@@ -191,8 +191,8 @@ const initiateApp = () => {
             },
             {
                 type: 'input',
-                name: 'title',
-                message: 'What is salary of this new role? ',
+                name: 'salary',
+                message: 'What is salary of this new role?',
                 validate: checkInput => {
                     if (checkInput){
                         return true; 
@@ -207,7 +207,7 @@ const initiateApp = () => {
                 type: 'list',
                 name: 'department_id',
                 message: 'What department is this new role assigned to? ',
-                choices: choicesDepartments
+                choices: choicesDepartments,
             }
         ])
 
